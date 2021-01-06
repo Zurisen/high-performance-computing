2bcf01aa2ec0f1d3c6926d51ea4c4830723d8cd8
carlos <mtcarlos98@gmail.com>
2021-01-06 21:09:42 +0100

    HPC files

--- week-1/mat_labs/main_mulblas.c
+++ week-1/mat_labs/main_mulblas.c
@@ -4,7 +4,7 @@
 
 #include "datatools.h"		/* helper functions	        */
 #include "lib.h"		/* my matrix add fucntion	*/
-#include <cblas-atlas.h>	/* Cblas */
+#include "cblas.h" 
 
 #define NREPEAT 100		/* repeat count for the experiment loop */
 
