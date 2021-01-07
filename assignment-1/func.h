6d70895bec82e751c8d6e16357f416e42964c3e0
raulorteg <mail.raulortega@gmail.com>
2021-01-07 12:28:37 +0100

    Cleaned func.h :art:

--- assignment-1/func.h
+++ assignment-1/func.h
@@ -1,10 +1,10 @@
 #ifndef __FUNC_LIB_H
 #define __FUNC_LIB_H
 
+/* orginal permutation for matrix multiplication (mnk) */
 void matmult_nat(int m, int n, int k, double **A, double **B, double **C);
-void matmult_lib(int m, int n, int k, double **A, double **B, double **C);
-void matmult_blk(int m, int n, int k, double **A, double **B, double **C, int bs);
-void matmult_nat(int m, int n, int k, double **A, double **B, double **C);
+
+/* permutations fro matrix multiplication */
 void matmult_mnk(int m, int n, int k, double **A, double **B, double **C);
 void matmult_mkn(int m, int n, int k, double **A, double **B, double **C);
 void matmult_nkm(int m, int n, int k, double **A, double **B, double **C);
@@ -12,5 +12,10 @@ void matmult_kmn(int m, int n, int k, double **A, double **B, double **C);
 void matmult_knm(int m, int n, int k, double **A, double **B, double **C);
 void matmult_nmk(int m, int n, int k, double **A, double **B, double **C);
 
-#endif
+/* native cblas implementation matrix multiplication */
+void matmult_lib(int m, int n, int k, double **A, double **B, double **C);
 
+/* matrix multiplication with batches */
+void matmult_blk(int m, int n, int k, double **A, double **B, double **C, int bs);
+
+#endif
