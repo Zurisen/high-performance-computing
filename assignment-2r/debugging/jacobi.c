b9b7266d524adfea1664648c008eb8c6671db548
S-Gutierrez <77106361+S-Gutierrez@users.noreply.github.com>
2021-01-13 22:08:57 +0100

    Add files via upload

---
+++ assignment-2r/debugging/jacobi.c
@@ -0,0 +1,69 @@
+/* jacobi.c - Poisson problem in 3d
+ * 
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void
+jacobi(double*** uNew, double*** uOld, double*** uSwap, double*** f, int N, int iter_max, double gridSpace, double tolerance) {
+    double invCube = 1/6;
+    double d=10e+5;
+
+
+    for (int iter = 0; (iter < iter_max && d > tolerance); iter++) {
+    	d = 0.0;
+
+
+
+    	for (int i = 1; i < N-1; i++) {
+    		for (int j = 1; j < N-1; j++) {
+    			for (int k = 1; k < N-1; k++) {
+
+    				/* Compute update of uNew */
+    				uNew[i][j][k] = (uOld[i-1][j][k] + uOld[i+1][j][k] + uOld[i][j-1][k] + uOld[i][j+1][k] + uOld[i][j][k-1] + uOld[i][j][k+1] + gridSpace*f[i][j][k]);
+					printf("%3.4f  ", uNew[i][j][k]);
+    				/* Compute new d */
+    				/* NOTE: maybe this can be parallelized better if 
+    				splitted into a different nested loop */
+    				d += abs(uNew[i][j][k] - uOld[i][j][k]);
+					
+    			}
+				printf("\n");
+    		}
+			printf("\n");
+			printf("\n");
+			printf("\n");
+
+			for (int j = 1; j < N-1; j++) {
+    			for (int k = 1; k < N-1; k++) {
+
+    				/* Compute update of uNew */
+					printf("%3.4f  ", uOld[i][j][k]);
+
+					
+    			}
+				printf("\n");
+			}
+			//printf("\n");
+			//printf("\n");
+			//for (int j = 1; j < N-1; j++) {
+    		//	for (int k = 1; k < N-1; k++) {
+
+    				/* Compute update of uNew */
+			//		printf("%3.4f  ", f[i][j][k]);
+
+					
+    		//	}
+			//	printf("\n");
+    		//}
+			printf("\n");
+			printf("\n");
+
+
+    	}
+		printf("distance : %8.8f", d);printf("\n");
+
+	}
+	
+}
