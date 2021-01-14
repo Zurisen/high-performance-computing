fb3856121b6c6144a55263f1b99fe5fecda20919
raulorteg <mail.raulortega@gmail.com>
2021-01-14 16:40:16 +0100

    splitted d loop in gauss-seidel :construction:

--- assignment-2r/gauss_seidel.c
+++ assignment-2r/gauss_seidel.c
@@ -9,32 +9,34 @@ void
 gauss_seidel(double*** uNew, double*** uOld, double*** uSwap, double*** f, int N, int iter_max, double gridSpace, double tolerance) {
     double invCube = 1/6.;
     double d=100000.0;
+    int i, j, k, iter;
 
-    for (int iter = 0; (iter < iter_max && d > tolerance); iter++) {
+    for (iter = 0; (iter < iter_max && d > tolerance); iter++) {
     	d = 0.0;
 
-        for (int i = 1; i < N-1; i++) {
-                for (int j = 1; j < N-1; j++) {
-                        for (int k = 1; k < N-1; k++) {
-                                uOld[i][j][k] = uNew[i][j][k];
-                        }
-                }
-        }
+        uSwap = uNew;
+        uNew = uOld;
+        uOld = uSwap;
 
-    	for (int i = 1; i < N-1; i++) {
-    		for (int j = 1; j < N-1; j++) {
-    			for (int k = 1; k < N-1; k++) {
+    	for (i = 1; i < N-1; i++) {
+    		for (j = 1; j < N-1; j++) {
+    			for (k = 1; k < N-1; k++) {
 
     				/* Compute update of uNew */
     				uNew[i][j][k] = invCube*(uNew[i-1][j][k] + uOld[i+1][j][k] 
     					+ uNew[i][j-1][k] + uOld[i][j+1][k] + uNew[i][j][k-1] + uOld[i][j][k+1] + gridSpace*f[i][j][k]);
 
-    				/* Compute new d */
-    				/* NOTE: maybe this can be parallelized better if 
-    				splitted into a different nested loop */
-    				d += abs(uNew[i][j][k] - uOld[i][j][k]);
     			}
     		}
     	}
+        for (i = 1; i < N-1; i++) {
+            for (j = 1; j < N-1; j++) {
+                for (k = 1; k < N-1; k++) {
+
+                    /* Compute new d */
+                    d += abs(uNew[i][j][k] - uOld[i][j][k]);
+                }
+            }
+        }
 	}
 }
