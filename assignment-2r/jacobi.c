87149389ed5786427a6a2c66e8b7a1d901f23606
raulorteg <mail.raulortega@gmail.com>
2021-01-14 14:33:36 +0100

    splitted d in jacobi.c :construction:

--- assignment-2r/jacobi.c
+++ assignment-2r/jacobi.c
@@ -11,13 +11,9 @@ jacobi(double*** uNew, double*** uOld, double*** uSwap, double*** f, int N, int 
     double d=100000.0;
    for (int iter = 0; (iter < iter_max || d > tolerance); iter++) {
         d = 0.0;
-        for (int i = 1; i < N-1; i++) {
-                for (int j = 1; j < N-1; j++) {
-                        for (int k = 1; k < N-1; k++) {
-				uOld[i][j][k] = uNew[i][j][k];
-			}
-		}
-	}
+        uSwap = uNew;
+        uNew = uOld;
+        uOld = uSwap;
 	
     	for (int i = 1; i < N-1; i++) {
     		for (int j = 1; j < N-1; j++) {
@@ -26,13 +22,18 @@ jacobi(double*** uNew, double*** uOld, double*** uSwap, double*** f, int N, int 
     				/* Compute update of uNew */
     				uNew[i][j][k] = invCube*(uOld[i-1][j][k] + uOld[i+1][j][k] 
     					+ uOld[i][j-1][k] + uOld[i][j+1][k] + uOld[i][j][k-1] + uOld[i][j][k+1] + gridSpace*f[i][j][k]);
-
-    				/* Compute new d */
-    				/* NOTE: maybe this can be parallelized better if 
-    				splitted into a different nested loop */
-				d += abs(uNew[i][j][k] - uOld[i][j][k]);
     			}
     		}
     	}
+
+        for (i = 1; i < N-1; i++) {
+            for (j = 1; j < N-1; j++) {
+                for (k = 1; k < N-1; k++) {
+
+                    /* Compute new d */
+                    d += abs(uNew[i][j][k] - uOld[i][j][k]);
+                }
+            }
+        }
 	}
 }
