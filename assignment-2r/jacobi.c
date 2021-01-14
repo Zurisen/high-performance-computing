7a9e8aefab88135c5133026813162f456f7f10fb
raulorteg <mail.raulortega@gmail.com>
2021-01-14 15:15:27 +0100

    update jacobi.c ints declared

--- assignment-2r/jacobi.c
+++ assignment-2r/jacobi.c
@@ -9,15 +9,16 @@ void
 jacobi(double*** uNew, double*** uOld, double*** uSwap, double*** f, int N, int iter_max, double gridSpace, double tolerance) {
     double invCube = 1/6.;
     double d=100000.0;
+int i, j, k;
    for (int iter = 0; (iter < iter_max || d > tolerance); iter++) {
         d = 0.0;
         uSwap = uNew;
         uNew = uOld;
         uOld = uSwap;
 	
-    	for (int i = 1; i < N-1; i++) {
-    		for (int j = 1; j < N-1; j++) {
-    			for (int k = 1; k < N-1; k++) {
+    	for (i = 1; i < N-1; i++) {
+    		for (j = 1; j < N-1; j++) {
+    			for (k = 1; k < N-1; k++) {
 
     				/* Compute update of uNew */
     				uNew[i][j][k] = invCube*(uOld[i-1][j][k] + uOld[i+1][j][k] 
