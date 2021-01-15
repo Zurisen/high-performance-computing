d7fbb55d67954bf9e3872df0c3fa6c635827ad83
raulorteg <mail.raulortega@gmail.com>
2021-01-15 19:24:07 +0100

    all plots :art:

--- assignment-2/plotter.py
+++ assignment-2/plotter.py
@@ -25,8 +25,7 @@ parent_folder = str(sys.argv[1]).split("/")[0]
 if not os.path.exists(parent_folder +'/plots'):
     os.makedirs(parent_folder +'/plots')
 
-data.columns = ["size_N", "iterations", "time", "iters_per_second", "memory", "mlups"]
-
+data.columns = ["size_N", "iterations", "time", "iters_per_second", "memory"]
 
 distinct_sizes_N = data.size_N.unique()
 
@@ -35,9 +34,42 @@ num_threads = [1, 2, 4, 8, 16]*len(distinct_sizes_N)
 data["num_threads"] = num_threads
 distinct_num_threads = [1, 2, 4, 8, 16]
 
+# compute speedup
+baseline_times = data[data["num_threads"] == 1].time.to_numpy()
+times_vect = data.time.to_numpy()
 
-print(data)
+speedup = []
+i = 0
+for times in times_vect:
+	if i<5:
+		speedup.append(baseline_times[0]/times)
+	elif i<10:
+		speedup.append(baseline_times[1]/times)
+	elif i<15:
+		speedup.append(baseline_times[2]/times)
+	elif i<20:
+		speedup.append(baseline_times[3]/times)
+	elif i<25:
+		speedup.append(baseline_times[4]/times)
+	elif i<30:
+		speedup.append(baseline_times[5]/times)
+	elif i<35:
+		speedup.append(baseline_times[6]/times)
+	else:
+		print("error in speedup loop")
+		break
+	i = i + 1
+data["speedup"] = speedup
 
+
+# compute mlups
+mlups = []
+memories = data.memory.to_numpy()
+for idx, memory in enumerate(memories):
+	mlups.append(memory/(8*1000*times_vect[idx]))
+data["mlups"] = mlups
+
+# print(data)
 
 # total time vs matrix size N
 fig = plt.figure()
@@ -130,7 +162,22 @@ plt.xlabel("Memory footprint (kBytes)")
 plt.ylabel("MLUPS")
 fig.savefig(parent_folder +'/plots' +'/mlupsVSmemory.png')
 
+# speedup vs num_threads
+fig = plt.figure()
+ax = plt.subplot(111)
+memories = data.memory.unique()
+for mem in memories:
+	speedup = data[data.memory == mem].speedup
+	num_threads = data[data.memory == mem].num_threads
+	ax.plot(num_threads, speedup, label=f'{mem} kBytes', marker="o")
+
+ax.legend()
+plt.xlabel("Number of threads")
+plt.ylabel("Speedup")
+fig.savefig(parent_folder +'/plots' +'/speedupVSnumthreads.png')
+
 
 print("Success!. Images saved at", parent_folder +'/plots')
+data.to_csv(parent_folder +'/plots' +'latex.txt', header=True, index=False, sep='&')
 exit()
 
