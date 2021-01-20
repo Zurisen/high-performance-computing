1576478e21ecffb107b1d4a4e4aedd0bc17cb93a
raulorteg <mail.raulortega@gmail.com>
2021-01-20 23:03:42 +0100

    Debugging :ant:

--- assignment-3/matmult_gpu/func.cu
+++ assignment-3/matmult_gpu/func.cu
@@ -35,6 +35,7 @@ void matmult_gpu1(int M, int N, int K, double* A, double *B, double* C) {
     int size_A = M*K*sizeof(double);
     int size_B = N*K*sizeof(double);
     int size_C = N*M*sizeof(double);
+    double max_val = 10.0;
 
     /* GPU: Allocate memory on device */
     printf("Allocating memory... \n");
@@ -93,12 +94,12 @@ __global__ void matmult_gpu2_kernel(int M, int N, int K, double* A, double* B, d
     }
 }
 
-void matmult_gpu2(int M, int N, int K, double* A, double *B, double* C) {
+void matmult_gpu2(int M, int N, int K, double* h_A, double* h_B, double* h_C) {
     double *d_A, *d_B, *d_C; // Device variables
-    double *h_A, *h_B, *h_C; // host vairables
     int size_A = M*K*sizeof(double);
     int size_B = N*K*sizeof(double);
     int size_C = N*M*sizeof(double);
+    double max_val = 10.0;
 
     /* GPU: Allocate memory on device */
     printf("Allocating memory... \n");
@@ -163,12 +164,12 @@ __global__ void matmult_gpu3_kernel(int M, int N, int K, double* A, double* B, d
     }
 }
 
-void matmult_gpu3(int M, int N, int K, double* A, double *B, double* C) {
+void matmult_gpu3(int M, int N, int K, double* h_A, double* h_B, double* h_C) {
     double *d_A, *d_B, *d_C; // Device variables
-    double *h_A, *h_B, *h_C; // host vairables
     int size_A = M*K*sizeof(double);
     int size_B = N*K*sizeof(double);
     int size_C = N*M*sizeof(double);
+    double max_val = 10.0;
 
     /* GPU: Allocate memory on device */
     printf("Allocating memory... \n");
@@ -236,23 +237,23 @@ __global__ void matmult_gpu4_kernel(int M, int N, int K, double* A, double* B, d
     }
 }
 
-void matmult_gpu4(int M, int N, int K, double* A, double *B, double* C) {
+void matmult_gpu4(int M, int N, int K, double* h_A, double* h_B, double* h_C) {
     double *d_A, *d_B, *d_C; // Device variables
-    double *h_A, *h_B, *h_C; // host vairables
     int size_A = M*K*sizeof(double);
     int size_B = N*K*sizeof(double);
     int size_C = N*M*sizeof(double);
+    double max_val = 10.0;
 
     /* GPU: Allocate memory on device */
     printf("Allocating memory... \n");
     cudaMalloc((void**)&d_A, size_A);
-    cudaMalloc((void**)&d_B, size_b);
-    cudaMalloc((void**)&d_C, size_c);
+    cudaMalloc((void**)&d_B, size_B);
+    cudaMalloc((void**)&d_C, size_C);
 
     /* GPU: Allocate memory on host */
     cudaMallocHost((void**)&h_A, size_A);
-    cudaMallocHost((void**)&h_B, size_b);
-    cudaMallocHost((void**)&h_C, size_c);
+    cudaMallocHost((void**)&h_B, size_B);
+    cudaMallocHost((void**)&h_C, size_C);
 
     /* Initialize matrices with random data (h_A. h_b)*/
     printf("Initializing matrices... \n");
@@ -286,67 +287,18 @@ void matmult_gpu4(int M, int N, int K, double* A, double *B, double* C) {
     printf("Freeing memory... \n");
     cudaFreeHost(h_A);
     cudaFreeHost(h_B);
-    cudaFreeHost(h_c);
+    cudaFreeHost(h_C);
     cudaFree(d_A);
     cudaFree(d_B);
     cudaFree(d_C);
 }
 /* part 5: GPU (shared memory version) */
-__global__ void matmult_gpu5(int M, int N, int K, double** A, double **B, double** C) {
-    double *d_A, *d_B, *d_C; // Device variables
-    double *h_A, *h_B, *h_C; // host vairables
-    int size_A = M*K*sizeof(double);
-    int size_B = N*K*sizeof(double);
-    int size_C = N*M*sizeof(double);
-
-    /* GPU: Allocate memory on device */
-    printf("Allocating memory... \n");
-    cudaMalloc((void**)&d_A, size_A);
-    cudaMalloc((void**)&d_B, size_b);
-    cudaMalloc((void**)&d_C, size_c);
-
-    /* GPU: Allocate memory on host */
-    cudaMallocHost((void**)&h_A, size_A);
-    cudaMallocHost((void**)&h_B, size_b);
-    cudaMallocHost((void**)&h_C, size_c);
-
-    /* Initialize matrices with random data (h_A. h_b)*/
-    printf("Initializing matrices... \n");
-    init_2d(max_val, M, K, h_A);
-    init_2d(max_val, K, N, h_B);
-
-    /* Copying data to device */
-    printf("Copying data to device... \n");
-    cudaMemcpy(d_A, h_A, size_A, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, h_B, size_B, cudaMemcpyHostToDevice);
-
-    /* MATRIX MULTIPLICATION */
-    printf("Computing Matrix multiplication... \n");
-    // Define grid and threads per block
-    // declare the number of blocks per grid and the number of threads per block
-    // use 1 to 512 threads per block
-
-
-    //TODO:
-
-    printf("Copying results back to host... \n");
-    cudaMemcpy(h_C, d_C, size_C, cudaMemcpyDeviceToHost);
-    cudaDeviceSynchronize();
-
-    /* Freeing memory */
-    printf("Freeing memory... \n");
-    cudaFreeHost(h_A);
-    cudaFreeHost(h_B);
-    cudaFreeHost(h_c);
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
+__global__ void matmult_gpu5() {
+  
 }
 
 /* part 6: DGEMM function for GPUs, NVIDIA */
-__global__ void matmult_gpulib(int M, int N, int K, double **A, double **B, double **C) {
-        double alpha = 1.0, beta = 0.0;
-        cublasDgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,M,N,K,alpha,A[0],K,B[0],N,beta,C[0],N);
+__global__ void matmult_gpulib() {
 }
 
 /* Initialization of 2d arrays */
