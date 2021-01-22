3f1700b7cd5815cab3accbb37b921aaa92268e10
S-Gutierrez <77106361+S-Gutierrez@users.noreply.github.com>
2021-01-22 21:43:53 +0100

    Add files via upload

--- assignment-3/matmult_gpu/submit.sh
+++ assignment-3/matmult_gpu/submit.sh
@@ -11,18 +11,16 @@
 module load cuda/11.1
 module load gcc/9.2.0
 
-OUTFILE="results.txt"
+OUTFILE="results_gpu.txt"
 rm $OUTFILE
 EXECUTABLE=matmult_f.nvcc
-method=gpu1
 declare -A ITER
-ITER=( [512]=100 [1024]=10 [2048]=1 [4096]=1 [8192]=1 [10240]=1 )
+ITER=([64]=100 [128]=100 [256]=100 [512]=100 [1024]=10 [2048]=1 [4096]=1 [8192]=1 [10240]=1 )
 
-
-for size in 512 1024 2048 4096 8192 #10240
+for method in gpu4 gpu5
 do
-    printf "${method} "
-    printf "${size} "
-    MFLOPS_MAX_IT=${ITER[${size}]} MATMULT_COMPARE=0 ./matmult_f.nvcc $method $size $size $size >> $OUTFILE
+	for size in 64 128 256 512 1024 2048 4096 8192
+	do
+    	MFLOPS_MAX_IT=${ITER[${size}]} MATMULT_COMPARE=0 ./matmult_f.nvcc $method $size $size $size >> $OUTFILE
+	done
 done
-
